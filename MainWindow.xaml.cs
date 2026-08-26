46d00c65e0d1216ef3b78c78bee2b2e815b67cb5
emretenoz <emretenoz@users.noreply.github.com>
2026-08-26 13:12:05 +0300

    Fix WPF build

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
